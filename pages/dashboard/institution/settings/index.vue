5243a8bc9fc6c48645f67016e7174240cb93c9bd
Qin Guan <qinguan20040914@gmail.com>
2023-08-07 23:54:21 +0800

    feat: institution settings

---
+++ pages/dashboard/institution/settings/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Divider from 'primevue/divider'
+
+const Toast = defineAsyncComponent(() => import('primevue/toast'))
+</script>
+
+<template>
+  <div>
+    <Toast />
+
+    <h2 text-lg font-medium>
+      Vouchers
+    </h2>
+    <p text-sm opacity-80>
+      List of all vouchers
+    </p>
+
+    <Divider :pt="{ root: { class: 'before:border-solid!' } }" />
+  </div>
+</template>
