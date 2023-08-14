a47d49835d1c1b781b0a587c9aff7973c0347f13
Sherlena Ngui <66990228+C4RR0T02@users.noreply.github.com>
2023-08-14 23:52:16 +0800

    feat: editing and deleting of vouchers (#18)
    Co-authored-by: Qin Guan <qinguan20040914@gmail.com>

--- components/voucher/List.vue
+++ components/voucher/List.vue
@@ -1,19 +1,113 @@
 <script setup lang="ts">
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
+import Button from 'primevue/button'
 import Skeleton from 'primevue/skeleton'
 import Toast from 'primevue/toast'
+import Dialog from 'primevue/dialog'
+import InputText from 'primevue/inputtext'
+import InputNumber from 'primevue/inputnumber'
+import Textarea from 'primevue/textarea'
 import { useToast } from 'primevue/usetoast'
+import type { DefaultVoucher } from '~/shared/types'
+
+const formData = reactive({
+  visible: false,
+  id: '',
+  name: '',
+  description: '',
+  pointsRequired: 0,
+})
 
 const route = useRoute()
 const toast = useToast()
 
 const { data: vouchers, isLoading: vouchersIsLoading, error: vouchersError } = useVouchers()
+const { mutate: updateMutate } = useUpdateVoucher()
+const { mutate: deleteMutate } = useDeleteVoucher()
+
+function onEditVoucher(voucher: DefaultVoucher) {
+  formData.id = voucher.id
+  formData.name = voucher.name
+  formData.description = voucher.description
+  formData.pointsRequired = voucher.pointsRequired
+  formData.visible = true
+}
+
+function updateVoucher() {
+  updateMutate({
+    id: formData.id,
+    name: formData.name,
+    description: formData.description,
+    pointsRequired: formData.pointsRequired,
+  },
+  {
+    onSuccess() {
+      toast.add({
+        summary: 'Voucher updated!',
+        severity: 'success',
+        life: 3000,
+      })
+      formData.visible = false
+    },
+  })
+}
+
+function deleteVoucher(voucherId: string) {
+  deleteMutate({
+    voucherId,
+  },
+  {
+    onSuccess() {
+      toast.add({
+        summary: 'Voucher deleted!',
+        severity: 'success',
+        life: 3000,
+      })
+    },
+  })
+}
 </script>
 
 <template>
   <div>
     <Toast />
+
+    <Dialog v-model:visible="formData.visible" modal header="Edit voucher" class="min-w-sm">
+      <form space-y-4 @submit.prevent="updateVoucher">
+        <div class="flex flex-col gap-2">
+          <label for="name">Name</label>
+          <InputText
+            id="name" v-model="formData.name" autofocus :required="true" placeholder="Food Club $5"
+            aria-describedby="name-help"
+          />
+          <small id="name-help" class="sr-only">Enter the name of the voucher</small>
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <label for="points">Points required</label>
+          <InputNumber
+            id="points" v-model="formData.pointsRequired" :required="true" placeholder="5000"
+            aria-describedby="points-help"
+          />
+          <small id="points-help" class="sr-only">Enter the points required to redeem the voucher</small>
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <label for="description">Description</label>
+          <Textarea
+            id="description" v-model="formData.description" placeholder="Use this $5 voucher to buy anything (chicken rice) at Food Club!"
+            aria-describedby="description-help"
+          />
+          <small id="description-help" class="sr-only">Enter the description of the voucher</small>
+        </div>
+
+        <div mt6>
+          <Button type="submit" size="small" label="Update" />
+        </div>
+      </form>
+    </Dialog>
+
     <Skeleton v-if="vouchersIsLoading" height="300px" />
     <LazyErrorCard v-else-if="vouchersError" v-bind="vouchersError" />
 
@@ -29,9 +123,17 @@ const { data: vouchers, isLoading: vouchersIsLoading, error: vouchersError } = u
 
       <DataTable v-else :value="vouchers">
         <Column field="id" header="ID" />
-        <Column field="name" header="Name" sortable style="width: 50%" />
-        <Column field="description" header="Description" />
-        <Column field="pointsRequired" header="Points" sortable style="20%" />
+        <Column field="name" header="Name" sortable />
+        <Column field="description" header="Description" style="width: 50%" />
+        <Column field="pointsRequired" header="Points" sortable style="width: 20%" />
+        <Column header="Actions">
+          <template #body="slotProps">
+            <div>
+              <Button size="small" text label="Edit" @click="onEditVoucher(slotProps.data)" />
+              <Button size="small" text severity="danger" label="Delete" @click="deleteVoucher(slotProps.data.id)" />
+            </div>
+          </template>
+        </Column>
       </DataTable>
     </template>
   </div>
--- shared/voucher.ts
+++ shared/voucher.ts
@@ -6,9 +6,17 @@ export const createVoucherInput = z.object({
   pointsRequired: z.number().min(0),
 })
 
+export const updateVoucherInput = z.object({
+  id: z.string().cuid(),
+  name: z.string().min(1),
+  description: z.string().min(1),
+  pointsRequired: z.number().min(0),
+})
+
 export const redeemVoucherInput = z.object({
   voucherId: z.string().cuid(),
 })
 
 export type CreateVoucherInput = z.infer<typeof createVoucherInput>
+export type UpdateVoucherInput = z.infer<typeof updateVoucherInput>
 export type RedeemVoucherInput = z.infer<typeof redeemVoucherInput>
